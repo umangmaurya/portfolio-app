022d5cf71e8f014a0ede21f458e657693828fb06
Umang Maurya <umangmaurya@outlook.com>
2019-06-08 20:13:27 +0530

    Updates

---
+++ precache-manifest.15292f0d695274ae363f648ac45eca27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063a010920e513a3ef2e624023823bd2",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "6ecf4b2c5d72a3724c4e",
+    "url": "/portfolio-app/static/css/main.a176e0b4.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "6ecf4b2c5d72a3724c4e",
+    "url": "/portfolio-app/static/js/main.9f697310.chunk.js"
+  },
+  {
+    "revision": "44340542c48e10c20cd6",
+    "url": "/portfolio-app/static/js/runtime~main.cb97d77a.js"
+  }
+]);
